37b4f67a937adb740f0954253c21e6b4e6f0e0ed
Makoto Ikegame <makoto@lenscope.com.br>
2020-04-18 20:58:28 -0300

    Makoto's company listing

---
+++ google_business/companies-makoto.py
@@ -0,0 +1,156 @@
+STORES = {
+#    "magazine-luiza": [
+#        "https://www.google.com/maps/place/Magazine+Luiza/@-23.5939582,-46.5939936,15z/data=!3m1!4b1!4m10!1m2!2m1!1smagazine+luiza!3m6!1s0x0:0xea0033d08a57a883!8m2!3d-23.5939584!4d-46.5852392!9m1!1b1"
+#    ],
+#    "openbox2":[
+#        "https://www.google.com/maps/place/Openbox2/@-23.5409951,-46.6464974,17z/data=!3m1!4b1!4m10!1m2!2m1!1sopenbox2!3m6!1s0x0:0xa2417375617f1139!8m2!3d-23.5409927!4d-46.6443014!9m1!1b1",
+#        "https://www.google.com/maps/place/Openbox2/@-23.5375,-46.731083,17z/data=!3m1!4b1!4m10!1m2!2m1!1sopenbox2!3m6!1s0x0:0xe3a1d65fa7cf6edd!8m2!3d-23.5374911!4d-46.7288876!9m1!1b1"
+#    ]
+
+#	"sylvia-design": [
+#		977 reviews
+#		"https://www.google.com.br/maps/place/Sylvia+Design/@-23.5119906,-46.6268904,17z/data=!3m1!4b1!4m10!1m2!2m1!1ssylvia+design!3m6!1s0x0:0xd27127c06e36659e!8m2!3d-23.5119933!4d-46.6247106!9m1!1b1"
+#		13 reviews ok
+#		"https://www.google.com.br/maps/place/Sylvia+Design/@-23.5349549,-46.6024344,17z/data=!3m1!4b1!4m10!1m2!2m1!1ssylvia+design!3m6!1s0x0:0x8cb2f1eb946d1876!8m2!3d-23.5349534!4d-46.6002488!9m1!1b1"
+#		367 reviews ok
+#		"https://www.google.com.br/maps/place/Sylvia+Design/@-23.5391825,-46.5776248,17z/data=!3m1!4b1!4m10!1m2!2m1!1ssylvia+design!3m6!1s0x0:0xbc6960d29f7bc9d7!8m2!3d-23.5391829!4d-46.5754437!9m1!1b1"
+# 		162 reviews ok
+#		"https://www.google.com.br/maps/place/Sylvia+Design+Diamond/@-23.5997497,-46.6880305,17z/data=!3m1!4b1!4m10!1m2!2m1!1ssylvia+design!3m6!1s0x0:0x8ff2a513383a1e9!8m2!3d-23.5997591!4d-46.6858435!9m1!1b1"
+# 		612 reviews ok
+#		"https://www.google.com.br/maps/place/Sylvia+Design/@-23.5870446,-46.7565679,17z/data=!3m1!4b1!4m10!1m2!2m1!1ssylvia+design!3m6!1s0x0:0xa12d922caa4e68e2!8m2!3d-23.5870365!4d-46.7543793!9m1!1b1"
+#	]
+
+#	"etna": [
+#		9,485 reviews ~900 ok
+#		"https://www.google.com.br/maps/place/Etna+-+Tatuap%C3%A9/@-23.5279693,-46.5805578,17z/data=!3m1!4b1!4m10!1m2!2m1!1sEtna!3m6!1s0x0:0x745732f75a4a206a!8m2!3d-23.5279497!4d-46.5783834!9m1!1b1"
+#		9,125 reviews ~700 ok
+#		"https://www.google.com.br/maps/place/Etna+-+Brooklin/@-23.6163541,-46.6981533,17z/data=!3m1!4b1!4m10!1m2!2m1!1sEtna!3m6!1s0x0:0x3dccaf0e37ea39ae!8m2!3d-23.6163536!4d-46.69595!9m1!1b1"
+#	]
+
+#	"mobly": [
+#		266 reviews OK
+#		"https://www.google.com.br/maps/place/Mobly+Outlet+Guarulhos+Dutra/@-23.4705237,-46.5201631,17z/data=!3m1!4b1!4m10!1m2!2m1!1smobly!3m6!1s0x0:0xa99481b4b515483d!8m2!3d-23.470529!4d-46.5179586!9m1!1b1",
+#		184 reviews OK
+#		"https://www.google.com.br/maps/place/Mobly+Outlet+Suzano/@-23.5372878,-46.3017763,17z/data=!3m1!4b1!4m10!1m2!2m1!1smobly!3m6!1s0x0:0x22a95589ba9f9cb0!8m2!3d-23.5372747!4d-46.2996054!9m1!1b1",
+#		15 reviews OK
+#		"https://www.google.com.br/maps/place/Parceiro+Mobly+Radial+Leste/@-23.5280168,-46.5106489,17z/data=!3m1!4b1!4m10!1m2!2m1!1smobly!3m6!1s0x0:0x8bd760dbaf1cb315!8m2!3d-23.5280284!4d-46.5084743!9m1!1b1",
+#		15 reviews OK
+#		"https://www.google.com.br/maps/place/Parceiro+Mobly+Penha/@-23.5249678,-46.552138,17z/data=!3m1!4b1!4m10!1m2!2m1!1smobly!3m6!1s0x0:0xcad739157c60950a!8m2!3d-23.5249592!4d-46.5499306!9m1!1b1",
+#		51 reviews OK
+#		"https://www.google.com.br/maps/place/Parceiro+Mobly+Vila+Maria/@-23.5223062,-46.6015053,17z/data=!3m1!4b1!4m10!1m2!2m1!1smobly!3m6!1s0x0:0xc2ff2d5e62b00486!8m2!3d-23.5223228!4d-46.5993261!9m1!1b1",
+#		2.132 reviews ~1k OK
+#		"https://www.google.com.br/maps/place/Loja+Mobly+-+Megastore/@-23.6368883,-46.7217972,17z/data=!4m10!1m2!2m1!1smobly!3m6!1s0x0:0xfe68fa0cb43ed94d!8m2!3d-23.6368973!4d-46.7196178!9m1!1b1"
+#		263 reviews OK
+#		"https://www.google.com.br/maps/place/Mobly+Outlet+Guarapiranga/@-23.6667069,-46.7219906,17z/data=!3m1!4b1!4m10!1m2!2m1!1smobly!3m6!1s0x0:0xb7e7eae0a96dd6a9!8m2!3d-23.6666947!4d-46.7197895!9m1!1b1"
+#	]
+
+#	"tokstok": [
+#		829 reviews OK
+#		"https://www.google.com.br/maps/place/Tok%26Stok+Guarulhos/@-23.4426658,-46.5427132,17z/data=!3m1!4b1!4m10!1m2!2m1!1stok%26stok!3m6!1s0x0:0x4f74ab9bf7625d7!8m2!3d-23.4426558!4d-46.5405321!9m1!1b1"
+#		3,371 reviews 1,140 reviews OK
+#		"https://www.google.com.br/maps/place/Tok%26Stok/@-23.5183524,-46.6178049,17z/data=!4m10!1m2!2m1!1stok%26stok!3m6!1s0x0:0x7ee395da88731a32!8m2!3d-23.5183485!4d-46.615634!9m1!1b1"
+#		712 reviews OK
+#		"https://www.google.com.br/maps/place/Tok%26Stok+Jardim+An%C3%A1lia+Franco/@-23.5611437,-46.5638406,17z/data=!3m1!5s0x94ce5c2a278a95d1:0x5413612e612baa35!4m10!1m2!2m1!1stok%26stok!3m6!1s0x94ce5bf83a59182d:0x762c1d089790f6f3!8m2!3d-23.5611486!4d-46.5616519!9m1!1b1"
+#		982 reviews OK
+#		"https://www.google.com.br/maps/place/Tok+%26+Stok/@-23.5788496,-46.5968763,17z/data=!3m1!5s0x94ce5bf86129de31:0xe2320e1217508c40!4m10!1m2!2m1!1stok%26stok!3m6!1s0x94ce6372d1e19505:0x82ebd29ca86193fa!8m2!3d-23.5788545!4d-46.5946876!9m1!1b1"
+#		717 reviews OK
+#		"https://www.google.com.br/maps/place/Tok%26Stok/@-23.5423844,-46.6593792,17z/data=!3m2!4b1!5s0x94ce583b88281a8f:0xb01df47da32e1500!4m10!1m2!2m1!1stok%26stok!3m6!1s0x0:0x54789baff6c2d6b4!8m2!3d-23.5423894!4d-46.657176!9m1!1b1"
+#		1,265 reviews ~960 OK
+#		"https://www.google.com.br/maps/place/Tok%26Stok+Pomp%C3%A9ia/@-23.526325,-46.6756144,17z/data=!3m1!4b1!4m10!1m2!2m1!1stok%26stok!3m6!1s0x0:0x6635536a6c5e97c0!8m2!3d-23.5263364!4d-46.6734409!9m1!1b1"
+#		318 reviews OK
+#		"https://www.google.com.br/maps/place/Tok%26Stok+-+Loja+de+Saldos/@-23.4939687,-46.8368953,17z/data=!3m1!4b1!4m10!1m2!2m1!1stok%26stok!3m6!1s0x0:0x7bbbdbf60b90d87a!8m2!3d-23.4939883!4d-46.8347168!9m1!1b1"
+#		618 reviews OK
+#		"https://www.google.com.br/maps/place/Tok%26Stok+Shopping+Iguatemi+Alphaville/@-23.5047103,-46.8507054,17z/data=!3m2!4b1!5s0x94cf02211940bacf:0xef822e58337677a0!4m10!1m2!2m1!1stok%26stok!3m6!1s0x0:0x2f1b82132bbf7748!8m2!3d-23.5047325!4d-46.8485355!9m1!1b1",
+#		277 reviews OK
+#		"https://www.google.com.br/maps/place/Tok%26Stok/@-23.5064731,-46.9305641,17z/data=!3m1!4b1!4m10!1m2!2m1!1stok%26stok!3m6!1s0x0:0x2c63d4d50734716!8m2!3d-23.5064641!4d-46.9283581!9m1!1b1"
+#		4.129 reviews 960 OK
+#		"https://www.google.com.br/maps/place/Tok%26Stok/@-23.5732304,-46.7035106,17z/data=!3m1!4b1!4m10!1m2!2m1!1stok%26stok!3m6!1s0x0:0xf596b50daeacd795!8m2!3d-23.573231!4d-46.7013359!9m1!1b1"
+#		2,197 reviews
+#		"https://www.google.com.br/maps/place/Tok%26Stok+Santa+Cruz/@-23.5993581,-46.6392467,17z/data=!3m2!4b1!5s0x94ce5a3178006cc9:0x1e3146974b64acf0!4m10!1m2!2m1!1stok%26stok!3m6!1s0x0:0xe237ea9cf1d85415!8m2!3d-23.5993462!4d-46.6370487!9m1!1b1"
+#		1,981 reviews
+#		"https://www.google.com.br/maps/place/Tok%26Stok+Ibirapuera/@-23.6080652,-46.6691737,17z/data=!3m1!4b1!4m10!1m2!2m1!1stok%26stok!3m6!1s0x0:0x6269584c5e111488!8m2!3d-23.6080763!4d-46.6670036!9m1!1b1"
+#		192 reviews
+#		"https://www.google.com.br/maps/place/Tok%26Stok+Shopping+D%26D/@-23.6091703,-46.6995759,17z/data=!3m2!4b1!5s0x94ce50ccde07df5f:0x152d19b3774ec168!4m10!1m2!2m1!1stok%26stok!3m6!1s0x0:0xcf3f981e98800a0e!8m2!3d-23.6091773!4d-46.6973877!9m1!1b1"
+#		381 reviews
+#		"https://www.google.com.br/maps/place/Tok%26Stok+Market+Place/@-23.6214992,-46.701831,17z/data=!3m2!4b1!5s0x94ce50db2dda602f:0x5413612ee45b981e!4m10!1m2!2m1!1stok%26stok!3m6!1s0x0:0x4635acbdef50959f!8m2!3d-23.621485!4d-46.6996622!9m1!1b1"
+#		748 reviews
+#		"https://www.google.com.br/maps/place/Tok%26Stok+Morumbi+Town/@-23.6319998,-46.7394566,17z/data=!3m1!4b1!4m10!1m2!2m1!1stok%26stok!3m6!1s0x0:0x27306be0d7e06e22!8m2!3d-23.6320222!4d-46.7372561!9m1!1b1"
+#		1,193 reviews
+#		"https://www.google.com.br/maps/place/Tok%26Stok+ABC/@-23.6677917,-46.5357523,17z/data=!3m2!4b1!5s0x94ce4260562cd10b:0xe2320e122a73b8d5!4m10!1m2!2m1!1stok%26stok!3m6!1s0x0:0x3f112d5e2de2156b!8m2!3d-23.6677952!4d-46.5335798!9m1!1b1"
+#	]
+
+#	"inovar": [
+#		9 reviews OK
+#		"https://www.google.com.br/maps/place/Inovar+M%C3%B3veis+e+Colch%C3%B5es/@-23.592111,-46.8349484,17z/data=!3m1!4b1!4m10!1m2!2m1!1sinovar+moveis!3m6!1s0x94cfab45f56f3935:0x3b136be06409c19!8m2!3d-23.5921159!4d-46.8327597!9m1!1b1",
+#		61 reviews OK
+#		"https://www.google.com.br/maps/place/Inovar+Moveis+e+Colch%C3%B5es/@-23.5301315,-46.3400865,17z/data=!3m1!4b1!4m10!1m2!2m1!1sinovar+moveis!3m6!1s0x94ce7ae5ef3b9aeb:0xd98f838bfc37f7b8!8m2!3d-23.5301364!4d-46.3378978!9m1!1b1",
+#		34 reviews OK
+#		"https://www.google.com.br/maps/place/Inovar+M%C3%B3veis+e+Colch%C3%B5es/@-23.6488788,-46.5359352,17z/data=!3m1!4b1!4m10!1m2!2m1!1sinovar+moveis!3m6!1s0x94ce42951d14f31d:0xb4a8923f042728d!8m2!3d-23.6488837!4d-46.5337465!9m1!1b1",
+#		3 reviews OK
+#		"https://www.google.com.br/maps/place/Inovar+M%C3%B3veis+e+Colch%C3%B5es/@-23.6637729,-46.4634518,17z/data=!4m10!1m2!2m1!1sinovar+moveis!3m6!1s0x94ce69838acc83e9:0x2689b36a7e70658!8m2!3d-23.6637778!4d-46.4612631!9m1!1b1",
+#		23 reviews OK
+#		"https://www.google.com.br/maps/place/Inovar+M%C3%B3veis+e+Colch%C3%B5es/@-23.5429021,-46.3166015,17z/data=!3m1!4b1!4m10!1m2!2m1!1sinovar+moveis!3m6!1s0x94ce7b3fae113e59:0xb5614a0880feef8e!8m2!3d-23.542907!4d-46.3144128!9m1!1b1",
+#		2 reviews OK
+#		"https://www.google.com.br/maps/place/Inovar+Moveis+e+Colch%C3%B5es/@-23.5944667,-46.7650917,17z/data=!4m10!1m2!2m1!1sinovar+moveis!3m6!1s0x94ce54348579a52b:0x3c102efb513a1a07!8m2!3d-23.5944716!4d-46.762903!9m1!1b1",
+#		7 reviews OK
+#		"https://www.google.com.br/maps/place/Inovar+M%C3%B3veis+e+Colch%C3%B5es/@-23.5348215,-46.7735994,17z/data=!3m1!4b1!4m10!1m2!2m1!1sinovar+moveis!3m6!1s0x94ceffca487c8209:0xa882e4ceb379a88f!8m2!3d-23.5348264!4d-46.7714107!9m1!1b1",
+#		21 reviews OK
+#		"https://www.google.com.br/maps/place/Inovar+M%C3%B3veis+e+Colch%C3%B5es/@-23.4808667,-46.6056863,17z/data=!3m1!4b1!4m10!1m2!2m1!1sinovar+moveis!3m6!1s0x94cef745eaef75f9:0xe484d51e2bc0c286!8m2!3d-23.4808716!4d-46.6034976!9m1!1b1",
+#		19 reviews OK
+#		"https://www.google.com.br/maps/place/Inovar+M%C3%B3veis+e+Colch%C3%B5es/@-23.5206308,-46.1975865,17z/data=!3m1!4b1!4m10!1m2!2m1!1sinovar+moveis!3m6!1s0x94cdd8114cbac42f:0x4627b39a4a2baec5!8m2!3d-23.5206357!4d-46.1953978!9m1!1b1",
+#		57 reviews OK
+#		"https://www.google.com.br/maps/place/Inovar+M%C3%B3veis+e+Colch%C3%B5es/@-23.5328165,-46.2124131,17z/data=!3m1!4b1!4m10!1m2!2m1!1sinovar+moveis!3m6!1s0x94ce77e012f196a5:0x3edc2cdad39c4460!8m2!3d-23.5328214!4d-46.2102244!9m1!1b1",
+#		2 reviews OK
+#		"https://www.google.com.br/maps/place/Inova+M%C3%B3veis+Planejados/@-23.6653691,-46.5316934,17z/data=!4m10!1m2!2m1!1sinovar+moveis!3m6!1s0x94ce42626a915b49:0xd73747eaed9c3643!8m2!3d-23.665374!4d-46.5295047!9m1!1b1",
+#		1 reviews OK
+#		"https://www.google.com.br/maps/place/Inovar+M%C3%B3veis+e+Colch%C3%B5es/@-23.475032,-46.3291025,17z/data=!4m10!1m2!2m1!1sinovar+moveis!3m6!1s0x94ce7d78df98c175:0xdf3df484b84263a2!8m2!3d-23.4750369!4d-46.3269138!9m1!1b1",
+#		2 reviews OK
+#		"https://www.google.com.br/maps/place/Inovar+M%C3%B3veis+e+Colch%C3%B5es/@-23.4887885,-46.5503813,17z/data=!4m10!1m2!2m1!1sinovar+moveis!3m6!1s0x94ce5f0bb1f3ca19:0xd9517778e94428f9!8m2!3d-23.4887934!4d-46.5481926!9m1!1b1",
+#		61 reviews OK
+#		"https://www.google.com.br/maps/place/Inovar+Moveis+e+Colch%C3%B5es/@-23.5301315,-46.3400865,17z/data=!3m1!4b1!4m10!1m2!2m1!1sinovar+moveis!3m6!1s0x94ce7ae5ef3b9aeb:0xd98f838bfc37f7b8!8m2!3d-23.5301364!4d-46.3378978!9m1!1b1",
+#		2 reviews OK
+#		"https://www.google.com.br/maps/place/Inovar+M%C3%B3veis+e+Colch%C3%B5es+Prime/@-23.5257883,-46.1961771,17z/data=!4m10!1m2!2m1!1sinovar+moveis!3m6!1s0x94cdd92a6305649d:0x7fc61eb56dbcd357!8m2!3d-23.5257932!4d-46.1939884!9m1!1b1"
+#	]
+
+#	"esplanada": [
+#		1 reviews FAIL
+#		"https://www.google.com.br/maps/place/Esplanada+M%C3%B3veis/@-23.4062751,-46.7563783,17z/data=!3m1!4b1!4m10!1m2!2m1!1sesplanada+moveis!3m6!1s0x0:0x15e836572dad92b6!8m2!3d-23.4062701!4d-46.7542076!9m1!1b1",
+#		42 reviews 
+#		"https://www.google.com.br/maps/place/Esplanada+M%C3%B3veis/@-23.4745204,-46.6692682,17z/data=!3m1!4b1!4m10!1m2!2m1!1sesplanada+moveis!3m6!1s0x0:0x50a75ac02b6aeeac!8m2!3d-23.4745048!4d-46.6670895!9m1!1b1",
+#		5 reviews OK
+#		"https://www.google.com.br/maps/place/Esplanada+M%C3%B3veis/@-23.5035222,-46.7725898,17z/data=!4m10!1m2!2m1!1sesplanada+moveis!3m6!1s0x0:0xf27ed5efa37d6908!8m2!3d-23.5035125!4d-46.7703867!9m1!1b1",
+#		74 reviews OK
+#		"https://www.google.com.br/maps/place/Esplanada+M%C3%B3veis/@-23.521175,-46.8362831,17z/data=!3m1!4b1!4m10!1m2!2m1!1sesplanada+moveis!3m6!1s0x0:0x5437de670861b434!8m2!3d-23.5211817!4d-46.8340731!9m1!1b1"
+#		30 reviews OK
+#		"https://www.google.com.br/maps/place/Esplanada+M%C3%B3veis/@-23.511597,-46.8784669,17z/data=!3m1!4b1!4m10!1m2!2m1!1sesplanada+moveis!3m6!1s0x0:0x3547170fecaf4a9c!8m2!3d-23.51158!4d-46.8762589!9m1!1b1",
+#		96 reviews OK
+#		"https://www.google.com.br/maps/place/Esplanada+M%C3%B3veis/@-23.4071913,-46.8933028,17z/data=!3m1!4b1!4m10!1m2!2m1!1sesplanada+moveis!3m6!1s0x0:0x25512d40e33bac51!8m2!3d-23.4071759!4d-46.8911076!9m1!1b1"
+#	]
+
+	"kingstar": [
+#		121 reviews OK
+		"https://www.google.com.br/maps/place/King+Star+Colch%C3%B5es/@-23.5594595,-46.4481861,17z/data=!3m1!4b1!4m10!1m2!2m1!1sking+star!3m6!1s0x94ce667d8f689afb:0xf5dd3e793266725e!8m2!3d-23.5594595!4d-46.4459974!9m1!1b1",
+#		176 reviews OK
+		"https://www.google.com.br/maps/place/King+Star+Colch%C3%B5es/@-23.495074,-46.4344887,17z/data=!3m1!4b1!4m10!1m2!2m1!1sking+star!3m6!1s0x0:0x5c2b6027038c05d4!8m2!3d-23.4950903!4d-46.4322996!9m1!1b1",
+#		122 reviews OK
+		"https://www.google.com.br/maps/place/King+Star+Colch%C3%B5es/@-23.457154,-46.5335417,17z/data=!3m1!4b1!4m10!1m2!2m1!1sking+star!3m6!1s0x0:0xda79a496e7873fc4!8m2!3d-23.4571443!4d-46.5313482!9m1!1b1",
+#		148 reviews OK
+		"https://www.google.com.br/maps/place/King+Star+Colch%C3%B5es/@-23.458029,-46.5649027,17z/data=!3m1!4b1!4m10!1m2!2m1!1sking+star!3m6!1s0x0:0x9d2f77e043fc54c8!8m2!3d-23.4580104!4d-46.5627193!9m1!1b1",
+#		153 reviews OK
+		"https://www.google.com.br/maps/place/King+Star+Colch%C3%B5es/@-23.5530589,-46.5648019,17z/data=!4m10!1m2!2m1!1sking+star!3m6!1s0x0:0xf13d50cac969d52b!8m2!3d-23.553051!4d-46.5626335!9m1!1b1",
+#		253 reviews OK
+		"https://www.google.com.br/maps/place/King+Star+Colch%C3%B5es/@-23.5032713,-46.6380053,17z/data=!3m1!4b1!4m10!1m2!2m1!1sking+star!3m6!1s0x0:0xf984d74b72c52981!8m2!3d-23.5032764!4d-46.6358042!9m1!1b1"
+#		 reviews
+#		"https://www.google.com.br/maps/place/Inovar+M%C3%B3veis+e+Colch%C3%B5es/@-23.5328165,-46.2124131,17z/data=!3m1!4b1!4m10!1m2!2m1!1sinovar+moveis!3m6!1s0x94ce77e012f196a5:0x3edc2cdad39c4460!8m2!3d-23.5328214!4d-46.2102244!9m1!1b1",
+#		 reviews
+#		"https://www.google.com.br/maps/place/Inova+M%C3%B3veis+Planejados/@-23.6653691,-46.5316934,17z/data=!4m10!1m2!2m1!1sinovar+moveis!3m6!1s0x94ce42626a915b49:0xd73747eaed9c3643!8m2!3d-23.665374!4d-46.5295047!9m1!1b1",
+#		 reviews
+#		"https://www.google.com.br/maps/place/Inovar+M%C3%B3veis+e+Colch%C3%B5es/@-23.475032,-46.3291025,17z/data=!4m10!1m2!2m1!1sinovar+moveis!3m6!1s0x94ce7d78df98c175:0xdf3df484b84263a2!8m2!3d-23.4750369!4d-46.3269138!9m1!1b1",
+#		 reviews
+#		"https://www.google.com.br/maps/place/Inovar+M%C3%B3veis+e+Colch%C3%B5es/@-23.4887885,-46.5503813,17z/data=!4m10!1m2!2m1!1sinovar+moveis!3m6!1s0x94ce5f0bb1f3ca19:0xd9517778e94428f9!8m2!3d-23.4887934!4d-46.5481926!9m1!1b1",
+#		 reviews
+#		"https://www.google.com.br/maps/place/Inovar+Moveis+e+Colch%C3%B5es/@-23.5301315,-46.3400865,17z/data=!3m1!4b1!4m10!1m2!2m1!1sinovar+moveis!3m6!1s0x94ce7ae5ef3b9aeb:0xd98f838bfc37f7b8!8m2!3d-23.5301364!4d-46.3378978!9m1!1b1",
+#		 reviews
+#		"https://www.google.com.br/maps/place/Inovar+M%C3%B3veis+e+Colch%C3%B5es+Prime/@-23.5257883,-46.1961771,17z/data=!4m10!1m2!2m1!1sinovar+moveis!3m6!1s0x94cdd92a6305649d:0x7fc61eb56dbcd357!8m2!3d-23.5257932!4d-46.1939884!9m1!1b1"
+	]
+}
